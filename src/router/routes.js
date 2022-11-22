792f7fa9e77fc34622ced655fb644363964aa853
voidrlm <varjun609@gmail.com>
2022-11-23 03:12:45 +0530

    Added user management feature

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,20 @@
 const routes = [
   {
+    path: "/",
+    name: "Login",
+    meta: { authorized: false },
+    component: () => import("../views/userAuthentication.vue"),
+  },
+  {
     path: "/dashboard",
     name: "Dashboard",
+    meta: { authorized: true },
     component: () => import("../views/dashboard.vue"),
   },
   {
     path: "/calendar",
     name: "Calendar",
+    meta: { authorized: true },
     component: () => import("../views/calendar.vue"),
   },
 ];
---
+++ src/services/sessionManagement.js
@@ -0,0 +1,20 @@
+import store from "@/store/index";
+import router from "../router";
+import { decrypt } from "../services/encryptDecrypt";
+export function initializeSession() {
+  if (
+    window.$cookies.get("uauth") !== null &&
+    JSON.stringify(window.$cookies.get("uauth")) !== JSON.stringify({})
+  ) {
+    var decrypted = decrypt(window.$cookies.get("uauth"));
+    store.dispatch("setCurrentUser", decrypted);
+  }
+  let currentUserSession = store.getters.currentUser;
+  if (JSON.stringify(currentUserSession) === JSON.stringify({})) {
+    if (router.currentRoute.path !== "/") {
+      router.push({
+        path: "/",
+      });
+    }
+  }
+}
