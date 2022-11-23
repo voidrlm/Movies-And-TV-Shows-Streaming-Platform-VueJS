0a33d71f3481dcdccc55d5ba43ad1b73e9f3f82b
voidrlm <varjun609@gmail.com>
2022-11-23 18:26:54 +0530

    Changes related to removal navigation drawer component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <v-app>
     <v-main
-      ><nav-bar v-if="$router.currentRoute.path !== '/'" /> <router-view />
+      ><appBar v-if="$router.currentRoute.path !== '/'" /> <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import navBar from "./components/navigation/navBar";
+import appBar from "./components/navigation/appBar.vue";
 import { initializeSession } from "./services/sessionManagement";
 export default {
   name: "App",
   components: {
-    navBar,
+    appBar,
   },
   data: () => ({
     //
