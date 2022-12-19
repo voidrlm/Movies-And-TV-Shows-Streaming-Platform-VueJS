0f3dd1791f3c4999fb4035cf4ccb5fc4c424f665
voidrlm <varjun609@gmail.com>
2022-12-19 21:28:27 +0530

    Added footer component

---
+++ src/components/navigation/footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-footer color="accent lighten-1" class="mt-5" padless>
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+          :href="link.link"
+          target="_blank"
+        >
+          {{ link.name }}
+        </v-btn>
+        <v-col class="accent lighten-2 py-5 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Created By Arjun.V</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer-component",
+  components: {},
+  data: () => ({
+    links: [
+      { name: "GitHub", link: "https://github.com/voidrlm" },
+      { name: "Twitter", link: "https://twitter.com/voidrlm" },
+    ],
+  }),
+
+  methods: {},
+};
+</script>
