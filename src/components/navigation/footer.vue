64c636aa3e69f3fc0e9e2f179688b4d489ef60d9
voidrlm <varjun609@gmail.com>
2022-12-21 01:42:19 +0530

    Minor fix in footer component

--- src/components/navigation/footer.vue
+++ src/components/navigation/footer.vue
@@ -3,8 +3,8 @@
     <v-footer color="accent lighten-1" class="mt-5" padless>
       <v-row justify="center" no-gutters>
         <v-btn
-          v-for="link in links"
-          :key="link"
+          v-for="(link, i) in links"
+          :key="i"
           color="white"
           text
           rounded
