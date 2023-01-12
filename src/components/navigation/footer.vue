3e2962f49609a4fc261f2614757cd9db3b213af2
voidrlm <varjun609@gmail.com>
2023-01-13 02:01:32 +0530

    Changes in author tag footer design

--- src/components/navigation/footer.vue
+++ src/components/navigation/footer.vue
@@ -1,23 +1,20 @@
 <template>
   <div>
-    <v-footer color="accent lighten-1" class="mt-5" padless>
-      <v-row justify="center" no-gutters>
-        <v-btn
-          v-for="(link, i) in links"
-          :key="i"
-          color="white"
-          text
-          rounded
-          class="my-2"
-          :href="link.link"
-          target="_blank"
-        >
-          {{ link.name }}
-        </v-btn>
-        <v-col class="accent lighten-2 py-5 text-center white--text" cols="12">
-          {{ new Date().getFullYear() }} — <strong>Created By Arjun.V</strong>
-        </v-col>
-      </v-row>
+    <v-footer color="accent lighten-1" class="mt-5" padless app dense>
+      <v-spacer />
+      <strong class="mr-3">Created By Arjun.V </strong>
+      <v-btn
+        v-for="(link, i) in links"
+        :key="i"
+        color="white"
+        text
+        rounded
+        :href="link.link"
+        target="_blank"
+      >
+        {{ link.name }}
+      </v-btn>
+      <v-spacer />
     </v-footer>
   </div>
 </template>
